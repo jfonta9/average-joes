efe806963f73ab7ca5f87052b3ad45abcaa26718
joe <fontanajoseph49@gmail.com>
2019-06-06 20:55:58 -0700

    express generator fix

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'average-joes',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-54-203-50-176.us-west-2.compute.amazonaws.com',
+      key: '~/.ssh/averagejoes.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:jfonta9/average-joes.git',
+      path: '/home/ubuntu/average-joes',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
