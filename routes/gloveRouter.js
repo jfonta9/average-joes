f8a0ab723dfc6228508c1ea465af814443e3a97d
joe <fontanajoseph49@gmail.com>
2019-06-11 11:16:43 -0700

    cors

--- routes/gloveRouter.js
+++ routes/gloveRouter.js
@@ -12,7 +12,7 @@ gloveRouter.use(bodyParser.json());
 gloveRouter.route('/')
 .options(cors.corsWithOptions, (req, res) => { res.sendStatus(200); })
 .get(cors.cors, (req,res,next) => {
-    Gloves.find({})
+    Gloves.find(req.query)
     .then((gloves) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
